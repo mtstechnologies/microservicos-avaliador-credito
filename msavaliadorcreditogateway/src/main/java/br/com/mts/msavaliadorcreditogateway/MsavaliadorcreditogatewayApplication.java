a7fe888f884fa61ea656d435a922048b5dbba097
“mtstechnologies” <michael.trindade@hotmail.com>
2024-11-29 16:47:18 -0400

    gateway created

---
+++ msavaliadorcreditogateway/src/main/java/br/com/mts/msavaliadorcreditogateway/MsavaliadorcreditogatewayApplication.java
@@ -0,0 +1,13 @@
+package br.com.mts.msavaliadorcreditogateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsavaliadorcreditogatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsavaliadorcreditogatewayApplication.class, args);
+	}
+
+}
---
+++ msavaliadorcreditogateway/src/test/java/br/com/mts/msavaliadorcreditogateway/MsavaliadorcreditogatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mts.msavaliadorcreditogateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsavaliadorcreditogatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
