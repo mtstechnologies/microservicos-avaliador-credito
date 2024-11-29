cd00eefea8e04e7189c17a8cedc9c0da3abbdc6e
“mtstechnologies” <michael.trindade@hotmail.com>
2024-11-29 13:38:25 -0400

    eureka server created

---
+++ msavaliadorcreditoeurekaserver/src/main/java/br/com/mts/msavaliadorcreditoeurekaserver/MsmsavaliadorcreditoeurekaserverApplication.java
@@ -0,0 +1,13 @@
+package br.com.mts.msavaliadorcreditoeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsmsavaliadorcreditoeurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsmsavaliadorcreditoeurekaserverApplication.class, args);
+	}
+
+}
---
+++ msavaliadorcreditoeurekaserver/src/test/java/br/com/mts/msavaliadorcreditoeurekaserver/MsmsavaliadorcreditoeurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mts.msavaliadorcreditoeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsmsavaliadorcreditoeurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
