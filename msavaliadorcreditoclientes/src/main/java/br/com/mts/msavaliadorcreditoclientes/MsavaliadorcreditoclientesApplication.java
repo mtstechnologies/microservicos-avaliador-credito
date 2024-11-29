9fd4e670caedab8c07aa23d46b5abd22ef9cc1c9
“mtstechnologies” <michael.trindade@hotmail.com>
2024-11-29 14:09:09 -0400

    microservices client created

---
+++ msavaliadorcreditoclientes/src/main/java/br/com/mts/msavaliadorcreditoclientes/MsavaliadorcreditoclientesApplication.java
@@ -0,0 +1,13 @@
+package br.com.mts.msavaliadorcreditoclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsavaliadorcreditoclientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsavaliadorcreditoclientesApplication.class, args);
+	}
+
+}
---
+++ msavaliadorcreditoclientes/src/test/java/br/com/mts/msavaliadorcreditoclientes/MsavaliadorcreditoclientesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mts.msavaliadorcreditoclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsavaliadorcreditoclientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
