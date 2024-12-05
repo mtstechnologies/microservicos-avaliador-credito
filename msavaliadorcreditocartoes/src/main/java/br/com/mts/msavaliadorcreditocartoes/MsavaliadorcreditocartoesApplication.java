160b6b0507453c6c14d31b2da772221cc17c9b86
“mtstechnologies” <michael.trindade@hotmail.com>
2024-12-05 14:35:33 -0400

    microservices card created

---
+++ msavaliadorcreditocartoes/src/main/java/br/com/mts/msavaliadorcreditocartoes/MsavaliadorcreditocartoesApplication.java
@@ -0,0 +1,13 @@
+package br.com.mts.msavaliadorcreditocartoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsavaliadorcreditocartoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsavaliadorcreditocartoesApplication.class, args);
+	}
+
+}
---
+++ msavaliadorcreditocartoes/src/test/java/br/com/mts/msavaliadorcreditocartoes/MsavaliadorcreditocartoesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mts.msavaliadorcreditocartoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsavaliadorcreditocartoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
