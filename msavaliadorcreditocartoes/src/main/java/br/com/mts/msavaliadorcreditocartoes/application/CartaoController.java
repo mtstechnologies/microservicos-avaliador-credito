b235a4078e2caf6f3b131c84e8aa3824c3c3911e
“mtstechnologies” <michael.trindade@hotmail.com>
2024-12-06 17:23:51 -0400

    microservice - card - get for income implemented

--- msavaliadorcreditocartoes/src/main/java/br/com/mts/msavaliadorcreditocartoes/application/CartaoController.java
+++ msavaliadorcreditocartoes/src/main/java/br/com/mts/msavaliadorcreditocartoes/application/CartaoController.java
@@ -1,11 +1,14 @@
 package br.com.mts.msavaliadorcreditocartoes.application;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.mts.msavaliadorcreditocartoes.application.dto.CartaoDto;
@@ -30,5 +33,11 @@ public class CartaoController {
 		Cartao cartao = dto.toModel();
 		cartaoService.save(cartao);
 		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
+	
+	@GetMapping(params = "renda")
+	public ResponseEntity<List<Cartao>> obterCartoesRendaAte(@RequestParam("renda") Long renda){
+		List<Cartao> list = cartaoService.getCartaoRendaMenorIgual(renda);
+		return ResponseEntity.ok(list);
 	}
 }
